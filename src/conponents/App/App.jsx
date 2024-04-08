d388a9ceab648a87ef28e674b195276e669dafe9
Volodymyr <110925314+woland31252@users.noreply.github.com>
2024-04-08 23:26:38 +0300

    create_SearchBox

--- src/conponents/App/App.jsx
+++ src/conponents/App/App.jsx
@@ -5,18 +5,18 @@ import css from '../App/App.module.css'
 import ContactList from '../ContactList/ContactList'
 import SearchBox from '../SearchBox/SearchBox'
 function App() {
-  const [findContact, setFindContact] = useState(contacts)
+  const [contact, setContact] = useState(contacts);
+  const [filter, setFilter] = useState("")
 
-  const handleFindContact = (evt) => {
-    const findElem = setFindContact(evt.target.value);
-    findContact.filter(elem => elem.findElem)
-  }
+  
+
+  const filterContact = contact.filter((cont) => cont.name.toLowerCase().includes(filter.toLowerCase()));
 
   return (
     <div className={css.container}>
       <h1 className={css.phoneTitle}>Phonebook</h1>
-      <SearchBox onFind={ handleFindContact} />
-      <ContactList contacts={findContact} />
+      <SearchBox value={filter} onFilter={ setFilter} />
+      <ContactList contacts={filterContact} />
     </div>
   )
 }
--- src/conponents/SearchBox/SearchBox.jsx
+++ src/conponents/SearchBox/SearchBox.jsx
@@ -1,9 +1,15 @@
+import { useId } from 'react'
 import css from '../SearchBox/SearchBox.module.css'
-export default function SearchBox() {
+export default function SearchBox({value, onFilter}) {
+    const findId = useId()
     return (
         <div className={css.findContainer}>
-            <label className={css.findLabel} htmlFor="">Find contacts by name</label>
-            <input className={css.findInput} type="text" name="search"/>
+            <label className={css.findLabel} htmlFor={findId}>Find contacts by name</label>
+            <input className={css.findInput}
+                type="text"
+                id={findId}
+                value={value}
+                onChange={(evt) => onFilter(evt.target.value)} />
         </div>
     )
 }
